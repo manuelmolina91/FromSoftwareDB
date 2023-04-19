262bd64ddb8bc1988d0ff8f5bbd61bfd696ceb96
Manuel Molina Ramirez <manuel.molina.ramirez1991@gmail.com>
2023-04-20 00:08:27 +0200

    feat: Creation of the index.js file

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("holaaaaa")
+const express = require('express')
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const app = express()
+
+const startApp = async () => {
+    dotenv.config()
+    app.use(cors())
+    const port = process.env.PORT | 8000
+
+    app.use(bodyParser.json())
+    app.use(
+        bodyParser.urlencoded({
+            extended: true,
+        })
+    )
+
+
+    try {
+        app.listen(port, () => {
+            console.log('APP running on port ' + port)
+        })
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+startApp()
